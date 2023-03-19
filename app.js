b5b0198d30b7fb7a78cf401ef7cac765d6bb0bc2
kenta-tkhs <takahashikenta0117@gmail.com>
2023-03-19 10:33:02 +0900

    0319

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mysql = require('mysql');
 const session = require('express-session');
 const bcrypt = require('bcrypt');
 const app = express();
+const ejs = require('ejs');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
@@ -14,8 +15,20 @@ const connection = mysql.createConnection({
   database: 'blog'
 });
 
-app.get('/', (req, res) =>{
+app.use(
+  session({
+    secret: 'my_secret_key',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.get('/', (req, res) => {
     res.render('index.ejs');
+});
+
+app.get('/setting', (req, res) => {
+  res.render('setting.ejs')
 });
 
 app.listen(3000);
