4106ee20329246a743b5c4a1848e0b7e1ffd9994
kenta-tkhs <takahashikenta0117@gmail.com>
2023-03-21 19:02:48 +0900

    n

--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,32 @@
-var count = 30;
+var count = 10;
 var sec = 0;
+var times = 4;
 var stp = null;
+var display = document.getElementById("default");
+
+display.innerHTML = count;
+
 
 function count_start(){
-    for (var i=0; i < 8; i++){
-        stp = setInterval(count_down,1000);
-    }
+    stp = setInterval(count_down,1000);
 }
 
 
 function count_down(){
-    if(count === 1){
-        var display = document.getElementById("default");
-        display.innerHTML = "TIME UP!";
-        clearInterval(stp);
-    } else {
-        count--;
-        sec = count % 60;
-        var count_down = document.getElementById("default");
-        count_down.innerHTML = sec;
-    }
+        if(count === 1){
+            if(times === 1){
+                display.innerHTML = "お疲れさまでした!";
+                clearInterval(stp);
+            }else{
+                count = 10;
+                times--;
+                display.innerHTML = (4-times)+"セット終了!";
+            }            
+        }else{
+            count--;
+            sec = count % 60;
+            var count_down = document.getElementById("default");
+            count_down.innerHTML = sec;
+        }
+
 }
