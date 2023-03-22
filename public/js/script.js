25a02e7686909fedbd660d5ed0c35fa5ddb43423
kenta-tkhs <takahashikenta0117@gmail.com>
2023-03-22 17:17:36 +0900

    d

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,34 @@
 var count = 10;
-var sec = 0;
+var sec = null;
+var rest = 5;
 var times = 4;
 var stp = null;
-var display = document.getElementById("default");
 
-display.innerHTML = count;
 
 
 function count_start(){
-    stp = setInterval(count_down,1000);
+    stp = setInterval(count_down, 1000);
 }
 
 
 function count_down(){
+    let rest_time = true;
+    const display = document.getElementById("default");
+    if(rest_time){
+        if(rest === 1){
+            if(times === 1){
+                display.innerHTML = "error";
+                clearInterval(stp);
+            }else{
+                rest = 5;
+                rest_time = false;
+                display.innerHTML = (4-times)+"休憩終了!";
+            }            
+        }else{
+            rest--;
+            display.innerHTML = rest;
+        }
+    }else{
         if(count === 1){
             if(times === 1){
                 display.innerHTML = "お疲れさまでした!";
@@ -21,12 +37,11 @@ function count_down(){
                 count = 10;
                 times--;
                 display.innerHTML = (4-times)+"セット終了!";
+                rest_time = True;
             }            
         }else{
             count--;
-            sec = count % 60;
-            var count_down = document.getElementById("default");
-            count_down.innerHTML = sec;
+            display.innerHTML = count;
         }
-
+    }
 }
