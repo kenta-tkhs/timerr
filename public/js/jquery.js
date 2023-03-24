3c7d43aa9539178cd57f08428c36c39b7b5819e1
kenta-tkhs <takahashikenta0117@gmail.com>
2023-03-24 18:52:51 +0900

    n

--- public/js/jquery.js
+++ public/js/jquery.js
@@ -4,3 +4,17 @@ $(function() {
       $('.slide-menu').toggleClass('active');
     });
 });
+
+$('.menu-item').click(function(){
+  var $answer = $(this).find('.menu-item-content');
+  if($answer.hasClass('active')){
+    $answer.removeClass('active');
+    $answer.slideUp();
+    $(this).find('span').text('+');
+    
+  }else{
+    $answer.addClass('active');
+    $answer.slideDown();
+    $(this).find('span').text('-');
+  }
+});
