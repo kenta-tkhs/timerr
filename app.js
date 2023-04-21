e91a8184f5fc52357ebf82f993ad63bb40595c26
kenta-tkhs <takahashikenta0117@gmail.com>
2023-04-21 23:02:24 +0900

    d

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const mysql = require('mysql2');
 const session = require('express-session');
 const bcrypt = require('bcrypt');
 const app = express();
-const ejs = require('ejs');
 
 
 app.use(express.static('public'));
@@ -55,7 +54,104 @@ app.get('/', (req, res) => {
 });
 
 app.get('/signup', (req, res) => {
-  res.render('signup.ejs')
+  res.render('signup.ejs', { errors: [] });
+});
+
+app.post('/signup',
+  (req, res, next) => {
+    console.log('入力値の空チェック');
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    const errors = [];
+
+    if (username === '') {
+      errors.push('ユーザー名が空です');
+    }
+
+    if (email === '') {
+      errors.push('メールアドレスが空です');
+    }
+
+    if (password === '') {
+      errors.push('パスワードが空です');
+    }
+
+    if (errors.length > 0) {
+      res.render('signup.ejs', { errors: errors });
+    } else {
+      next();
+    }
+  },
+  (req, res, next) => {
+    console.log('メールアドレスの重複チェック');
+    const email = req.body.email;
+    const errors = [];
+    connection.query(
+      'SELECT * FROM users',
+      [email],
+      (error, results) => {
+        if (results.length > 0) {
+          errors.push('ユーザー登録に失敗しました');
+          res.render('signup.ejs', { errors: errors });
+        } else {
+          next();
+        }
+      }
+    );
+  },
+  (req, res) => {
+    console.log('ユーザー登録');
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    bcrypt.hash(password, 10, (error, hash) => {
+      connection.query(
+        //'INSERT INTO users (username, email, password) VALUES (?, ?, ?)',//
+        'INSERT INTO users VALUES ?',
+        [username, email, hash],
+        (error, results) => {
+          req.session.userId = results.insertId;
+          req.session.username = username;
+        }
+      );
+    });
+  }
+);
+
+app.get('/login', (req, res) => {
+  res.render('login.ejs');
+});
+
+app.post('/login', (req, res) => {
+  const email = req.body.email;
+  connection.query(
+    'SELECT * FROM users WHERE email = ?',
+    [email],
+    (error, results) => {
+      if (results.length > 0) {
+        const plain = req.body.password;
+        const hash = results[0].password;
+        bcrypt.compare(plain, hash, (error, isEqual) => {
+          if (isEqual) {
+            req.session.userId = results[0].id;
+            req.session.username = results[0].username;
+            res.redirect('/list');
+          } else {
+            res.render('login.ejs');
+          }
+        });
+      } else {
+        res.redirect('/login');
+      }
+    }
+  );
+});
+
+app.get('/logout', (req, res) => {
+  req.session.destroy((error) => {
+    res.redirect('/list');
+  });
 });
 
 app.listen(3000);
